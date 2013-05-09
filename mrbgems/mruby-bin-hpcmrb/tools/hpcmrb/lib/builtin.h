1094c522235100458287f8d9fab1ff863594147b
tomykaira <tomykaira@gmail.com>
2013-05-09 12:46:05 +0900

    Add declaration of missing builtin functions

--- mrbgems/mruby-bin-hpcmrb/tools/hpcmrb/lib/builtin.h
+++ mrbgems/mruby-bin-hpcmrb/tools/hpcmrb/lib/builtin.h
@@ -27,3 +27,13 @@ mrb_value print_1(mrb_value __self__, mrb_value n);
 
 mrb_value hpc_ary_aget_1(mrb_value __self__, mrb_value index);
 mrb_value hpc_ary_aset_2(mrb_value __self__, mrb_value index, mrb_value value);
+
+mrb_value sqrt_1(mrb_value __self__, mrb_value);
+mrb_value num_uminus_0(mrb_value __self__);
+mrb_value to_i_0(mrb_value __self__);
+mrb_value to_f_0(mrb_value __self__);
+mrb_value chr_0(mrb_value __self__);
+mrb_value rand_0(mrb_value __self__);
+mrb_value cos_1(mrb_value __self__, mrb_value);
+mrb_value sin_1(mrb_value __self__, mrb_value);
+mrb_value to_s_0(mrb_value __self__);
