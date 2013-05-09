62d9d0301dd74c0bd474bea8fd6ff3bcf08ce006
tomykaira <tomykaira@gmail.com>
2013-05-09 12:19:59 +0900

    Rename -@ to num_uminus

--- mrbgems/mruby-bin-hpcmrb/tools/hpcmrb/codegen.c
+++ mrbgems/mruby-bin-hpcmrb/tools/hpcmrb/codegen.c
@@ -209,6 +209,7 @@ put_operator_name(hpc_codegen_context *c, mrb_sym sym)
     {"!", "mrb_bob_not"},
     {"[]", "hpc_ary_aget"},
     {"[]=", "hpc_ary_aset"},
+    {"-@", "num_uminus"},
     {"", ""}
   };
 
